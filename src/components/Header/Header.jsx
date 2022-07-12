d91a6fed176e740b0ca257396ef1b8a78f443568
PJ Katema <panashe.katema@gmail.com>
2022-07-12 18:00:22 +0100

    Added active class to links in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
 
 import '../../assets/css/components/Header.css'
 const logo = require('../../assets/img/Pharm4All.png');
@@ -16,11 +17,11 @@ function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end mx-auto" id="responsive-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link href="/About">About Us</Nav.Link>
-                        <Nav.Link href="/Locations">Locations</Nav.Link>
-                        <Nav.Link href="/AvailabilityChecker">Availability Checker</Nav.Link>
-                        <Nav.Link href="/MedInfo">Med Info</Nav.Link>
-                        <Nav.Link href="/Login">Login</Nav.Link>
+                        <Nav.Link as={NavLink} to="/About">About Us</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Locations">Locations</Nav.Link>
+                        <Nav.Link as={NavLink} to="/AvailabilityChecker">Availability Checker</Nav.Link>
+                        <Nav.Link as={NavLink} to="/MedInfo">Med Info</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Login">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 
