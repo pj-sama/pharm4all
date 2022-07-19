dd08c873b42aa78a3ea3f05339d9c89c6de82055
PJ Katema <panashe.katema@gmail.com>
2022-07-19 16:39:09 +0100

    Fixed Modal Issue, Modals now display when modal is clicked

--- src/components/Stores/Stores.jsx
+++ src/components/Stores/Stores.jsx
@@ -1,61 +1,53 @@
-import React, { useState} from 'react'
+import React, { useState} from 'react';
+import { Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './Stores.css'
 
 function Stores() {
-  return (
-    <div>
-          <div class="list-group">
-              <a href="#exampleModal" class="list-group-item list-group-item-action" id="store" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  Store Name
-              </a>
-              <a href="#" class="list-group-item list-group-item-action" id="store" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  Store Name
-              </a>
-              <a href="#" class="list-group-item list-group-item-action" id="store" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  Store Name
-              </a>
-              <a href="#" class="list-group-item list-group-item-action" id="store" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  Store Name
-              </a>
-              <a href="#" class="list-group-item list-group-item-action" id="store" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  Store Name
-              </a>
-          </div>
+	const [show, setShow] = useState(false);
 
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-              aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Branch Name</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                              aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="d-flex justify-content-between">
-                            <p> Mon-Fri</p>
-                            <p> 09:00-18:00</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p> Sat</p>
-                            <p> 09:00-18:00</p>
-                        </div>
-                            <div class="d-flex justify-content-between">
-                                <p> Sun (inc. Holidays)</p>
-                                <p> 09:00-18:00</p>
-                            </div>
-                        </div>
-                        <div class="modal-footer justify-content-around">
-                            <p class="text-center">
-                                <i class="fa-solid fa-phone"></i>
-                                0111 111 111
-                            </p>
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	
+	return (
+		<>
+			<ListGroup>
+				<Button variant="link" onClick={handleShow}>
+					<ListGroupItem className='list-group-item-action'>
+						Store Name
+					</ListGroupItem>
+      			</Button>
+			</ListGroup>
 
-                            <a href="#"><i class="fa-solid fa-location-dot"></i> View on map</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+			{/* Store Info Modal  */}
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header>
+					<Modal.Title> <h4>Store Name</h4> </Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Row>
+						<Col>Mon-Fri</Col>
+						<Col>09:00-20:00</Col>
+					</Row>
+					<Row>
+						<Col>Sat</Col>
+						<Col>09:00-18:00</Col>
+					</Row>
+					<Row>
+						<Col>Sun (incl. Holidays) </Col>
+						<Col>10:00-16:00</Col>
+					</Row>
+				</Modal.Body>
+				<Modal.Footer>
+					<p class="text-center"><i class="fa-solid fa-phone"></i> 0111 111 111</p>
+					<a href="/"><i class="fa-solid fa-location-dot"></i> View on map</a>
+				</Modal.Footer>
+
+			</Modal>
+			
+		</>
   )
 }
 
