39be1e55a973291d793966911921f5097b2af6b5
PJ Katema <panashe.katema@gmail.com>
2022-09-11 18:24:20 +0100

    promise preview

--- components/promisepreview/PromisePreview.jsx
+++ components/promisepreview/PromisePreview.jsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
+
 import { Container, Button } from 'react-bootstrap'
+
+import styles from '../../styles/PromisePreview.module.css'
+
 function PromisePreview() {
   return (
-    <div id='our_promise' className='blue-bg'>
+    <div id={styles.promise} className='blue-bg'>
         <Container fluid='md'  className='text-center'>
             <h2 className="display-5">Our Promise</h2>
             <p className="m-3 fs-4"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nobis sed iusto
@@ -11,7 +15,7 @@ function PromisePreview() {
               alias quae. Ipsam, maiores.</p>
         
             <Button variant="light" className='fs-5 text-black'>
-                <Link href='/About' className='text-muted'>
+                <Link href='/About' id={styles.findoutmore}className='text-muted'>
                     Find out more
                 </Link>
             </Button>{' '}
