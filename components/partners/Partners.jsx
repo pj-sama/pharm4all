6d8551f2cb4c9df62632474d544dbe1c6bdee920
PJ Katema <panashe.katema@gmail.com>
2022-09-11 17:06:40 +0100

    Partners section and styling

--- components/partners/Partners.jsx
+++ components/partners/Partners.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 
 import { Container, Row , Col } from 'react-bootstrap'
 
+// Stylesheet
+import styles from '../../styles/Partners.module.css'
+
 // Image imports
 import Astrazeneca from '/public/images/partners/astrazeneca.png'
 import Biogen from '/public/images/partners/biogen.png'
@@ -15,28 +18,70 @@ import Stryker from '/public/images/partners/stryker.png'
 
 function Partners() {
     return (
-        <Container fluid="md" className="py-5 d-flex flex-column align-items-center justify-content-center text-center" id = "our_partners">
+        <Container fluid="md" className="py-5 d-flex flex-column align-items-center justify-content-center text-center" id= {styles.partners}>
             <h2 className="display-5">Our Partners</h2>
             <Row className='my-4'>
                 <Col md={4}>
-                    <Link href="/"><Image src={Johnson_and_johnson} className="img-fluid" alt="Johnson and Johnson logo"/></Link>
+                    <Link href="/">
+                        <Image 
+                            src={Johnson_and_johnson}
+                            className="img-fluid"
+                            alt="Johnson and Johnson logo"
+                            id={styles.partnersimg}
+                            />
+                    </Link>
                 </Col>
                 <Col md={4}>
-                    <Link href="/"><Image src={Pfizer} className="img-fluid" alt="Pfizer logo"/></Link>
+                    <Link href="/">
+                        <Image
+                            src={Pfizer}
+                            className="img-fluid" 
+                            alt="Pfizer logo"
+                            id={styles.partnersimg}
+                        />
+                    </Link>
                 </Col>
                 <Col md={4}>
-                    <Link href="/"><Image src={Astrazeneca} className="img-fluid" alt="Pfizer-logo logo"/></Link>
+                    <Link href="/">
+                        <Image 
+                            src={Astrazeneca} 
+                            className="img-fluid" 
+                            alt="Pfizer-logo logo"
+                            id={styles.partnersimg}
+                        />
+                    </Link>
                 </Col>
             </Row>
             <Row>
                 <Col md={4}>
-                    <Link href="/"><Image src={Biogen} className="img-fluid" alt="Biogen logo" /></Link>
+                    <Link href="/">
+                        <Image 
+                            src={Biogen} 
+                            className="img-fluid" 
+                            alt="Biogen logo" 
+                            id={styles.partnersimg}
+                            />
+                    </Link>
                 </Col>
                 <Col md={4}>
-                    <Link href="/"><Image src={Stryker} className="img-fluid" alt="Stryker logo" /></Link>
+                    <Link href="/">
+                        <Image 
+                            src={Stryker} 
+                            className="img-fluid" 
+                            alt="Stryker logo" 
+                            id={styles.partnersimg}
+                            />
+                    </Link>
                 </Col>
                 <Col md={4}>
-                    <Link href="/"><Image src={Cipla} className="img-fluid" alt="Cipla logo" /></Link>
+                    <Link href="/">
+                        <Image 
+                            src={Cipla} 
+                            className="img-fluid" 
+                            alt="Cipla logo" 
+                            id={styles.partnersimg}
+                        />
+                    </Link>
                 </Col>
             </Row>
         </Container>
