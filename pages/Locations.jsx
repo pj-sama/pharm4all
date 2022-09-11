01b902aa10207b203117ec886b2f6e9f99322cb4
PJ Katema <panashe.katema@gmail.com>
2022-09-11 19:17:21 +0100

    Locations

---
+++ pages/Locations.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Component imports
+import Map from '../components/map/Map'
+import Stores from '../components/stores/Stores'
+
+function Locations() {
+  return (
+    <>
+     <Map />
+     <Stores />
+    </>
+  )
+}
+
+export default Locations
